b9d9c54196e31b064746f47ebd719ed828c4eb0b
tinnguyen <tinn1855@gmail.com>
2025-08-12 15:37:30 +0700

    feat(grid-movies): build UI gird movies

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import MoviesGrid from "./components/MoviesGrid";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <Header />
+        <MoviesGrid />
       </div>
       <Footer />
     </div>
