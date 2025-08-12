5b3591b4c5f6a3f8ff61b6e1168c1dd128c3b49c
tinnguyen <tinn1855@gmail.com>
2025-08-12 14:34:17 +0700

    feat(footer): build UI footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
       <div className="container">
         <Header />
       </div>
+      <Footer />
     </div>
   );
 }
