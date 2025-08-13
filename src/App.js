5c96a55089ee8ffaa7135831c9ef0c25f6bbfd2c
tinnguyen <tinn1855@gmail.com>
2025-08-13 09:50:22 +0700

    feat(router): install and config router

--- src/App.js
+++ src/App.js
@@ -2,13 +2,31 @@ import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import MoviesGrid from "./components/MoviesGrid";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Watchlist from "./components/Watchlist";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <Header />
-        <MoviesGrid />
+        <Router>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/watchlist">Watchlist</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/" element={<MoviesGrid />} />
+            <Route path="/watchlist" element={<Watchlist />} />
+            <Route path="*" element={<>Page not found</>} />
+          </Routes>
+        </Router>
       </div>
       <Footer />
     </div>
